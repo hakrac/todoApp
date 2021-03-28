03ca7361792d2305ac316e3b00604fe29ca8baff
hakrac <hakimrachidi02@gmail.com>
2021-03-28 22:24:11 +0200

    Update package.json

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ const path = require('path')
 module.exports = {
 	setupFiles: [
 		path.resolve(__dirname, './src/polyfills.js')
-	]
+	],
+	moduleNameMapper: {
+		"\\.(css|scss|sass)": path.resolve(__dirname, '__mocks__/style.js')
+	}
 }
